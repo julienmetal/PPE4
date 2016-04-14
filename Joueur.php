130a34c1c64a47844751c3b8b01c50853bc568c3
julienmetal <julien.lefevre.72@gmail.com>
2016-04-14 22:06:09 +0200

    Update Joueur.php

---
+++ Joueur.php
@@ -0,0 +1,22 @@
+<?php
+public class Personnage
+{
+  /**
+  * @Column(type="string")
+  **/
+  private $nom;
+  
+  
+  /**
+  * @Column(type="text")
+  **/
+  private $prenom;
+  
+  
+  /**
+  * @Column(type="string")
+  **/
+  private $age;
+}
+
+?>
