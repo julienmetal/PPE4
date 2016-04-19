2982e39ede45ff605dd58e9ed4f42a87871ff5db
julienmetal <julien.lefevre.72@gmail.com>
2016-04-19 14:22:40 +0200

    Ajout d'informations projet et codage de Sexe.php

---
+++ Sexe.php
@@ -0,0 +1,43 @@
+<?php
+/**
+* @Entity @Table(name="sexe")
+**/
+class Sexe
+{
+
+  /**
+  * @Column (type="string")
+  **/
+  private libelle
+
+  /**
+  * @Column (type="string")
+  **/
+  private
+
+  /**
+  * @Column (type="bool")
+  **/
+  //private $masculin
+
+  /**
+  * @Column (type="string")
+  **/
+  //private $feminin
+
+  /**
+  * @Column (type="string")
+  **/
+  //private $hermaphrodite
+
+  /**
+  * @Column (type="string")
+  **/
+  //private $autre
+
+  /**
+  * @Column (type="string")
+  **/
+  //private $assexué
+}
+ ?>
